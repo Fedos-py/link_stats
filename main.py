e6fe3e3ff5ff6beab0892e13b2ec6dbc0f1b2971
fedos <fedos@gmail.com>
2023-11-01 16:29:25 +0300

    initial commit

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return 'Привет, мир! Эта страница - начало моего проекта Link Statistic. dev: @Fedos-py'
+
+
+if __name__ == "__main__":
+    app.run()
