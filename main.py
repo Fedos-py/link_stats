484b1114970dac2b6dbfb24d446c23193fa62c1e
fedos <fedos@gmail.com>
2023-11-05 13:45:31 +0300

    feat: add /about

--- main.py
+++ main.py
@@ -8,5 +8,12 @@ def index():
     return 'Привет, мир! Эта страница - начало моего проекта Link Statistic. dev: @Fedos-py'
 
 
+@app.route('/about')
+def about():
+    return '<center>Link Statistic - проект, в котором будут реализован функционал для укорочения ссылок' \
+           ' и мониторинга статистики перехода по ним.<br><a href="https://t.me/fedospy">' \
+           'Связь с разработчиком</a></center>'
+
+
 if __name__ == "__main__":
     app.run()
